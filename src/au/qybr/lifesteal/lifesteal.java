dce6125f58af36982acab27bf6484b16703ed4e3
BuildTools <unconfigured@null.spigotmc.org>
2022-07-03 14:05:50 +1000

    enabled command, ready for bugtesting release 1

--- src/au/qybr/lifesteal/lifesteal.java
+++ src/au/qybr/lifesteal/lifesteal.java
@@ -6,6 +6,8 @@ public class lifesteal extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Lifesteal enabled!");
+        this.getCommand("withdraw").setExecutor(new iscommand());
+
     }
     @Override
     public void onDisable() {
