a95c1b120954bfaa88c627c1f2262e808fdbf487
BuildTools <unconfigured@null.spigotmc.org>
2022-07-03 11:34:23 +1000

    Expanded playerDeath with Hashmap

--- src/au/qybr/lifesteal/playerDeath.java
+++ src/au/qybr/lifesteal/playerDeath.java
@@ -6,13 +6,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.net.http.WebSocket;
+import java.util.HashMap;
+
 
 public class playerDeath implements Listener {
+    public HashMap<Player, Integer> playerlives = new HashMap<Player, Integer>();
     public void onDeath(PlayerDeathEvent e) {
         Entity killer = e.getEntity().getKiller();
         Player player = e.getEntity();
         if (killer instanceof org.bukkit.entity.Player) {
             Player killerPlayer = (Player) killer;
+
 
         }
     }
